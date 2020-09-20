a0b2c9d2f399ead0a1b4ad20e5c9daa7c107dc01
Kara Capps <kmcapps@Karas-MacBook-Pro.local>
2020-09-20 14:22:56 -0500

    initial commit

---
+++ SurvivalHorrorr/Assets/Scripts/FlameAnimations.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameAnimations : MonoBehaviour
+{
+    public int lightMode;
+    public GameObject FlameLight;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lightMode == 0)
+        {
+            StartCoroutine(AnimateLight());
+        }
+
+    }
+
+    IEnumerator AnimateLight()
+    {
+        lightMode = Random.Range(1, 4);
+        if (lightMode == 1)
+        {
+            FlameLight.GetComponent<Animation>().Play("TorchAnim1");
+        }
+        if (lightMode == 2)
+        {
+            FlameLight.GetComponent<Animation>().Play("TorchAnim2");
+        }
+        if (lightMode == 3)
+        {
+            FlameLight.GetComponent<Animation>().Play("TorchAnim3");
+        }
+        yield return new WaitForSeconds(0.99f);
+        lightMode = 0;
+    }
+}
---
+++ SurvivalHorrorr/Assets/Scripts/Sequences/BFirstTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+
+public class BFirstTrigger : MonoBehaviour
+{
+    public GameObject ThePlayer;
+    public GameObject TextBox;
+    public GameObject TheMarker;
+
+    private void OnTriggerEnter()
+    {
+        ThePlayer.GetComponent<FirstPersonController>().enabled = false;
+        StartCoroutine(ScenePlayer());
+    }
+
+    IEnumerator ScenePlayer()
+    {
+        TextBox.GetComponent<Text>().text = "What's on that table over there?";
+        yield return new WaitForSeconds(2.5f);
+        TextBox.GetComponent<Text>().text = "";
+        ThePlayer.GetComponent<FirstPersonController>().enabled = true;
+        TheMarker.SetActive(true);
+    }
+}
